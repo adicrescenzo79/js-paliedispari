d1363de5783f17ae8c6b0d37f9ca6a44011cc499
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-08 20:58:40 +0100

    completo con css e html

--- ParieDispari/assets/js/main.js
+++ ParieDispari/assets/js/main.js
@@ -4,9 +4,9 @@ function inizio() {
 
   scelta = prompt("P-Pari o D-Dispari?").toLowerCase();
 
-  console.log(scelta);
-
   if ((scelta=="d") || (scelta=="p")) {
+
+    scrviScelta();
 
     numero();
 
@@ -14,9 +14,14 @@ function inizio() {
 
       console.log(numero1);
 
+      document.getElementById("nr1").innerHTML = numero1;
+
+
       var numero2 = Math.floor(Math.random() * 5) + 1;
 
       console.log(numero2);
+
+      document.getElementById("nr2").innerHTML = numero2;
 
       giochiamo(numero1, numero2);
 
@@ -45,13 +50,25 @@ function giochiamo(nr1, nr2) {
 
     console.log("Hai vinto!");
 
+    document.getElementById("risultato").innerHTML = "Hai vinto!";
+
+    document.body.style.background = "green";
+
   } else if (!(somma%2) && (scelta=="p")) {
 
     console.log("Hai vinto!");
 
+    document.getElementById("risultato").innerHTML = "Hai vinto!";
+
+    document.body.style.background = "green";
+
   } else {
 
     console.log("Hai perso!");
+
+    document.getElementById("risultato").innerHTML = "Hai perso!";
+
+    document.body.style.background = "red";
   }
 }
 
@@ -59,4 +76,12 @@ function numero() {
 
   numero1 = parseInt(prompt("Dammi un numero da 1 a 5:"));
 
+}
+
+function scrviScelta() {
+  if ((scelta=="d")) {
+    document.getElementById("scelta").innerHTML = "Hai scelto DISPARI";
+  } else {
+    document.getElementById("scelta").innerHTML = "Hai scelto PARI";
+  }
 }
